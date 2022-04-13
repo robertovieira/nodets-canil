b505013a4b186d8444bad5fbccbbc38de3043831
Roberto Vieira <roberto.vieira@outlook.com>
2022-04-13 15:04:36 -0300

    Heroku setup

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Pet } from '../models/Pet';
+import { Pet } from '../models/pet';
 import { createMenuObject } from '../helpers/createMenuObjects';
 
 export const home = (req: Request, res: Response) => {
