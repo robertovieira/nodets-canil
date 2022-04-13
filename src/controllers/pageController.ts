33c72b73f5ee306777c4f754aee4d52c2d210e0b
Roberto Vieira <roberto.vieira@outlook.com>
2022-04-13 00:59:54 -0300

    Base criada

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res: Response) => {
+    res.send('Home no Controller!');
+}
+
+export const dogs = (req: Request, res: Response) => {
+
+}
+
+export const cats = (req: Request, res: Response) => {
+
+}
+
+export const fishies = (req: Request, res: Response) => {
+
+}
